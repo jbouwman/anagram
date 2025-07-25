c99ecd67b6dbd878c6f73df3e38d437b7e8ca155
Jesse Bouwman <jesse@cmdzero.io>
2025-07-25 08:22:48 +0000

    simplification

--- tests/anagram-tests.lisp
+++ tests/anagram-tests.lisp
@@ -1,16 +1,45 @@
-(defpackage anagram.tests
+(defpackage anagram.unit-tests
   (:use cl)
   (:local-nicknames
    (test epsilon.test)
-   (http epsilon.http.client)
    (json epsilon.json)
+   (http epsilon.http.client)
    (map epsilon.map))
   (:export run-tests))
 
-(in-package anagram.tests)
+(in-package anagram.unit-tests)
 
 (defparameter *test-port* 8081)
 (defparameter *base-url* (format nil "http://localhost:~A" *test-port*))
+
+(test:deftest test-shuffle-string ()
+  "Test the shuffle-string function"
+  (let ((original "hello")
+        (shuffled (anagram:shuffle-string "hello")))
+    ;; Should be same length
+    (test:is (= (length original) (length shuffled)))
+    ;; Should contain same characters (but allow same result occasionally)
+    (test:is (every (lambda (char) (find char shuffled)) original))
+    (test:is (every (lambda (char) (find char original)) shuffled))))
+
+(test:deftest test-compute-anagram ()
+  "Test the compute-anagram function"
+  (let* ((original "hello world")
+         (anagram (anagram:compute-anagram original)))
+    ;; Should be same length  
+    (test:is (= (length original) (length anagram)))
+    ;; Should contain same characters
+    (test:is-equal (sort (copy-seq original) #'char<)
+                   (sort (copy-seq anagram) #'char<))))
+
+(test:deftest test-make-json-response ()
+  "Test JSON response creation"
+  (let* ((data (map:make-map "message" "hello"))
+         (response (anagram:make-json-response data :status 200)))
+    (test:is (= (anagram:response-status response) 200))
+    (test:is-equal (map:get (anagram:response-headers response) "Content-Type")
+                   "application/json")
+    (test:is (stringp (anagram:response-body response)))))
 
 (test:deftest test-health-endpoint ()
   "Test the health check endpoint"
@@ -63,7 +92,7 @@
       (test:is (= status-code 400))
       (let ((body-data (json:parse body)))
         (test:is (map:get body-data "error"))))))
-
+!
 (test:deftest test-invalid-json ()
   "Test anagram generation with invalid JSON"
   (multiple-value-bind (status-code headers body)
@@ -81,26 +110,3 @@
       (http:get (format nil "~A/nonexistent" *base-url*))
     (declare (ignore headers body))
     (test:is (= status-code 404))))
-
-(defun run-tests ()
-  "Run all anagram service tests"
-  (format t "~&Starting anagram service tests on port ~A...~%" *test-port*)
-  
-  ;; Start the service in a separate thread
-  (let ((server-thread (sb-thread:make-thread
-                        (lambda ()
-                          (anagram:start-service 
-                           :port *test-port* 
-                           :address "127.0.0.1"))
-                        :name "test-server")))
-    
-    ;; Give server time to start
-    (sleep 1)
-    
-    ;; Run the tests
-    (unwind-protect
-         (test:run :package "anagram.tests")
-      
-      ;; Stop the server
-      (sb-thread:terminate-thread server-thread)
-      (anagram:stop-service))))
